61a224a0c2fa0a5a1172fb720a1e7703c4d79cfd
Nam <namhoai167@gmail.com>
2023-02-27 22:35:27 +0700

    Add sample group API

--- main.go
+++ main.go
@@ -22,6 +22,12 @@ func main() {
 	e.GET("/admin", handler.Hello, isLogedIn, isAdmin)
 	e.POST("/login", handler.Login, middleware.BasicAuth(mdw.BasicAuth))
 
+	// Group API
+	groupUser := e.Group("/api/user", isLogedIn)
+	groupUser.GET("/", handler.GetUser)
+	groupUser.PUT("/", handler.UpdateUser, isAdmin)
+	groupUser.DELETE("/", handler.DeleteUser, isAdmin)
+
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
