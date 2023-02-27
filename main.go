98998f4eef408983c29e1854beff908ba850c54e
unknown <namhoai167@gmail.com>
2023-02-27 14:29:18 +0700

    Add simple login route logic

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+const TOKEN = "RETURNED_TOKEN"
+const USERNAME = "admin"
+const PASSWORD = "passwd"
 
 func main() {
 	// Echo instance
@@ -17,6 +22,7 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.POST("/login", login)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -30,7 +36,36 @@ func hello(c echo.Context) error {
 	return c.JSON(http.StatusOK, simpleResponse)
 }
 
+func login(c echo.Context) error {
+	// Init a new struct
+	request := new(LoginResquest)
+
+	// Bind the request context to struct
+	c.Bind(request)
+
+	// Log
+	log.Printf("req data %+v", request)
+
+	// Simple verification
+	if request.Username != USERNAME || request.Password != PASSWORD {
+		return c.JSON(http.StatusUnauthorized, nil)
+	}
+
+	return c.JSON(http.StatusOK, &LoginResponse{
+		Token: TOKEN,
+	})
+}
+
 // Interface
+type LoginResquest struct {
+	Username string `json:"username" query:"username" form:"username" xml:"username"`
+	Password string `json:"password" query:"password" form:"password" xml:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
 type SimpleResponse struct {
 	Text string `json:"text"`
 }
