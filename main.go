b596f91400cc23d83b9465c3e7b4d3e5c292fb39
unknown <namhoai167@gmail.com>
2023-02-27 14:06:06 +0700

    Create simple hello world JSON response for GET

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	simpleResponse := &SimpleResponse{
+		Text: "Hello world",
+	}
+	return c.JSON(http.StatusOK, simpleResponse)
+}
+
+// Interface
+type SimpleResponse struct {
+	Text string `json:"text"`
+}
